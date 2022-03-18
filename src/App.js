dc846cb864907f8cc88c7862c9e7d8d8fb5a9ddf
MSI\LAPTOP MSI <vantaytvt@gmail.com>
2022-03-18 16:53:22 +0700

    initial todo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Login from './todo/Login';
+import Todo from './todo/Todo';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './bootstrap.min.css'
+
+ReactDOM.render(
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  document.getElementById('root')
+);
